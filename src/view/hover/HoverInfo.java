1a02f01c16eb681f5f46d1c104063886dd1d277e
Philip Tallo <tallopt@mail.uc.edu>
2018-09-06 14:48:23 -0400

    automated width detection and size of HoverInfo

--- src/view/hover/HoverInfo.java
+++ src/view/hover/HoverInfo.java
@@ -2,26 +2,77 @@ package view.hover;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public abstract class HoverInfo<T> {
 
+    private final int fontSize = 14;
+    private final int textBuffer = 6;
+    private final Font font = new Font("Arial", fontSize);
+
     abstract ArrayList<String> getInfoKeys(T item);
     abstract HashMap<String, String> getInfoHashMap(T item);
 
     public void showInfo(double x, double y, GraphicsContext gc, T item) {
-        gc.setFill(Color.BLACK);
-        gc.fillRect(x, y, 100, 50);
-
         HashMap<String, String> infoMap = getInfoHashMap(item);
         ArrayList<String> infoKeys = getInfoKeys(item);
 
-        gc.setFill(Color.LIGHTGREY);
+        double width = determineWidth(item);
+        double height = determinHeight(item);
+        double xMargin = 6;
+
+        gc.setFill(Color.LIGHTGRAY);
+        gc.fillRect(x, y, width, height);
+
+        gc.setLineWidth(3);
+        gc.setStroke(Color.GRAY);
+        gc.strokeRect(x, y, width, height);
+
+        gc.setFill(new Color(0, 0, 0, 1));
+        gc.setFont(font);
         for (int i = 0; i < infoKeys.size(); i++) {
-            String infoText = infoKeys.get(i) + ": " + infoMap.get(infoKeys.get(i));
-            gc.fillText(infoText, x + 3, y + (i * 10), 94);
+            String infoText = getString(i, item);
+            gc.fillText(infoText, x + xMargin, y + ((i + 1) * (fontSize + textBuffer)), width - (xMargin * 2));
         }
+    }
+
+    private String getString(int i, T item) {
+        HashMap<String, String> infoMap = getInfoHashMap(item);
+        ArrayList<String> infoKeys = getInfoKeys(item);
+        return infoKeys.get(i) + ": " + infoMap.get(infoKeys.get(i));
+    }
+
+    private double determinHeight(T item) {
+        int rows = getInfoKeys(item).size();
+        return (fontSize * rows) + (textBuffer * (rows + 1));
+    }
+
+    private double determineWidth(T item) {
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<String> keys = getInfoKeys(item);
+
+        for (int i = 0; i < keys.size(); i++) {
+            strings.add(getString(i, item));
+        }
+
+        double maxWidth = 0;
+        for (String str : strings){
+            double width = getWidth(str);
+            if (width > maxWidth){
+                maxWidth = width;
+            }
+        }
+
+        return maxWidth;
+    }
+
+    public double getWidth(String string){
+        Text text = new Text(string);
+        text.setFont(font);
+        return text.getLayoutBounds().getWidth();
     }
 }
